cb131ab7a29abd71b7717e2c992b2e51226a1b45
Tyler Miller <tdm77095@yahoo.com>
2020-08-29 17:35:25 -0700

    Updates

---
+++ precache-manifest.8d1f80c6a1661a5640eff79a73790a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673f6dc2753d9741921e84414b704832",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "76aec1c66a7749734268",
+    "url": "/monsters-rolodex/static/css/main.5aa3000a.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76aec1c66a7749734268",
+    "url": "/monsters-rolodex/static/js/main.97f93295.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
